a68b13b868056f0a560c2002c802ec9c0d90949c
macarena sotelo <macarenasotelo1@gmail.com>
2022-09-21 05:13:28 +0200

    funcionan los button y agregan al carrito

--- js/main.js
+++ js/main.js
@@ -1,35 +1,26 @@
-
+const carrito =[];
 const productos = [
-    {id: 1, nombre : "Labial 24hs matte" , marca : "Maybelline", precio : 1500},
-    {id: 2, nombre : "Labial 24 hs" , marca : "Maybelline", precio :1600},
-    {id: 3, nombre : "Labial matte" , marca : "Maybelline", precio :1300},
-    {id: 4, nombre : "Labial hidratante" , marca : "Maybelline", precio :1000},
-    {id: 5, nombre : "Labial perlado" , marca : "Maybelline", precio :1100},
-    {id: 6, nombre : "Mascara de pestañas" , marca : "Maybelline", precio :1500},
-    {id: 7, nombre : "Mascara de pestañas a prueba de agua" , marca : "Maybelline", precio :1600},
-    {id: 8, nombre : "Mascara de pestañas" , marca : "Maybelline", precio :1100},
-    {id: 9, nombre : "Mascara de pestañas larga duracion" , marca : "Maybelline", precio :1500},
+    {id: 1, nombre : "Labial 24hs matte" , marca : "Maybelline", precio : 1500, img: "articulo1.jpg"},
+    {id: 2, nombre : "Labial 24 hs" , marca : "Maybelline", precio :1600, img: "articulo3.jpg"},
+    {id: 3, nombre : "Labial matte" , marca : "Maybelline", precio :1300 , img: "articulo3.jpg"},
+    {id: 4, nombre : "Labial hidratante" , marca : "Maybelline", precio :1000, img: "articulo4.jpg"},
+    {id: 5, nombre : "Labial perlado" , marca : "Maybelline", precio :1100, img: "articulo5.jpg"},
+    {id: 6, nombre : "Mascara de pestañas" , marca : "Maybelline", precio :1500, img: "articulo7.jpg"},
+    {id: 8, nombre : "Mascara de pestañas" , marca : "Maybelline", precio :1100, img: "articulo1.jpg"},
+    {id: 9, nombre : "Mascara de pestañas larga duracion" , marca : "Maybelline", precio :1500, img: "articulo11.jpg "},
 ];
 
-function ProductosInfo(id,nombre,marca,precio){
+function ProductosInfo(id,nombre,marca,precio,img){
     this.id = id;
     this.nombre = nombre;
     this.marca = marca;
     this.precio = parseFloat(precio);
+    this.img = img;
 };
 
-const nuevoProducto = new ProductosInfo( 10,"Crema facial hidratante", "Avon",800);
-const nuevoProducto2 = new ProductosInfo( 11,"Crema facial hidratante","Revlon",1100);
-const nuevoProducto3 = new ProductosInfo( 12,"Crema facial hidratante", "Cicatricure",900);
-const nuevoProducto4 = new ProductosInfo( 13,"Crema facial hidratante", "Nivea",850);
-const nuevoProducto5 = new ProductosInfo( 14,"Crema facial hidratante", "Pons",800);
-const nuevoProducto6= new ProductosInfo ( 15,"Crema facial anti-edad", "Avon",1100);
-const nuevoProducto7 = new ProductosInfo( 16,"Crema facial hidratante", "Natura",100);
-const nuevoProducto8 = new ProductosInfo( 17,"Crema facial hidratante", "Dermaglos",1300);
-const nuevoProducto9 = new ProductosInfo( 18,"Crema facial anti-arrugas","Dermaglos",800);
-const nuevoProducto10 = new ProductosInfo( 19,"Crema facial hidratante", "La roche",800);
-
-console.log(productos);
+const nuevoProducto = new ProductosInfo( 10,"Crema facial hidratante", "Avon",800, "articulo10.jpg");
+const nuevoProducto2 = new ProductosInfo( 11,"Crema facial hidratante","Revlon",1100,"articulo10.jpg");
+//Function cargar productos al array principal
 
 function cargarArticulos(arr,element) {
     arr.push(element);
@@ -37,40 +28,18 @@ function cargarArticulos(arr,element) {
 
 cargarArticulos(productos,nuevoProducto);
 cargarArticulos(productos,nuevoProducto2);
-cargarArticulos(productos,nuevoProducto3);
-cargarArticulos(productos,nuevoProducto4);
-cargarArticulos(productos,nuevoProducto5);
-cargarArticulos(productos,nuevoProducto6);
-cargarArticulos(productos,nuevoProducto7);
-cargarArticulos(productos,nuevoProducto8);
-cargarArticulos(productos,nuevoProducto9);
-cargarArticulos(productos,nuevoProducto10);
+console.log(productos);
 
 
+/*Function Busqueda por Nombre
 function busquedaporNombre(arr,filtro) {
     const nombreBusqueda = arr.filter ((el) => {
         return el.nombre.includes(filtro)
     });
     return nombreBusqueda;
-}
+}  */
 
-
-//const resultado = busquedaporNombre(productos,ingreso);
-//console.log(resultado);
-
-//Primer paso para el carrito de compras
-
-carrito = [];
-
- //for (const element of resultado){
-  //  carrito.push(element);
- //}
-
-busquedaporNombre(carrito,resultado);
-console.log(carrito);
-
-let filtroprecio = prompt("Selecciona una opcion para mejorar tu busqueda: \n 1) Menor precio \n2) Mayor precio");
-
+/*flitrar por precio
 function filtrarporPrecio(arr,filtro){
    return arr.filter ((element) =>{
     switch (filtro) {
@@ -80,52 +49,47 @@ function filtrarporPrecio(arr,filtro){
             return element.precio > 1000;
     }
    });
+} */
+
+// const precio = filtrarporPrecio(productos,filtroprecio);
+ // console.log(precio)
+
+ const cajaTarjetas = document.querySelector(".tarjetas");
+ const cajaCarrito = document.querySelector(".carrito");
+
+ function crearCards(){
+    productos.forEach(element=>{
+        cajaTarjetas.innerHTML += `<div class = "tarjeta">
+        <img src="../images/${element.img}" alt="">
+        <h4>${element.nombre}</h4>
+        <p>$${element.precio}</p>
+        <button class= "btnCarrito" id="btn-agregar${element.id}" >Agregar</button>
+        </div>`
+    });
 }
 
-const precio = filtrarporPrecio(productos,filtroprecio);
-console.log(precio);
 
-/*   const ul = document.getElementById("lista");
-for (const producto of productos) {
-    let li = document.createElement("li");
-    li.innerHTML=
-   `<h3>${producto.nombre}</h3>
-    <p>${producto.marca}</p>
-    <span>${producto.precio}</span> `
-    ul.append(li);
-}                               */
+function agregarFuncionAlBoton(){
+    productos.forEach(element=>{
+        document.querySelector(`#btn-agregar${element.id}`).addEventListener("click",()=>{
+            agregarAlCarrito(element) ;
+        })
+    })
+}
 
-
-const comprar = document.querySelector("#btn-compra");
-const carro = document.querySelector("#mostrar-carrito");
-
-
-function crearHtml(arr) {
-    let html = "";
-    for (const item of arr) {
-      html = `<div>
-                  <h6>${item.titulo}</h6>
-                  <p>${item.precio}</p>
-              </div>`;
-      div.innerHTML += html;
+function agregarAlCarrito(producto){
+    /* console.log(producto.id); */
+    let existe = carrito.some(prod=>prod.id === producto.id);
+    if(existe===false){
+        producto.cantidad = 1;
+        carrito.push(producto);
     }
-}
+    else{
+        let prodFind = carrito.find(prod=> prod.id===producto.id);
+        prodFind.cantidad++;
+    }
+    console.log(carrito);
+    }
 
-
-function cargarCarro(arr,obj) {
-    return arr.push(obj);
-}
-
-//Listeners
-comprar.addEventListener("click", () => {
-    const nuevaCompra = new compra(
-      titulo.value,
-      precio.value,
-    );
-    console.log(nuevaCompra);
-  
-    cargarCarro(carrito, nuevaCompra);
-    //resetar el html de la tabla
-    tbody.innerHTML = "";
-    crearHtml(carrito);
-  });
+    crearCards();
+    agregarFuncionAlBoton(); 
