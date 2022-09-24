98dd6581632811c7bccebee976658e215aa92f02
macarena sotelo <macarenasotelo1@gmail.com>
2022-09-24 15:44:01 +0200

    agrego carrito y filtro

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-const carrito =[];
+const carrito= JSON.parse(localStorage.getItem("carrito")) || [];
+
+const search = document.querySelector("#input-search");
+const cajaTarjetas = document.querySelector(".tarjetas");
+ const cajaCarrito = document.querySelector(".carrito");
+ const nuevoProducto = new ProductosInfo( 10,"Crema facial hidratante", "Avon",800, "articulo10.jpg");
+ const nuevoProducto2 = new ProductosInfo( 11,"Crema facial hidratante","Revlon",1100,"articulo10.jpg");
+ 
 const productos = [
     {id: 1, nombre : "Labial 24hs matte" , marca : "Maybelline", precio : 1500, img: "articulo1.jpg"},
     {id: 2, nombre : "Labial 24 hs" , marca : "Maybelline", precio :1600, img: "articulo3.jpg"},
@@ -18,26 +25,13 @@ function ProductosInfo(id,nombre,marca,precio,img){
     this.img = img;
 };
 
-const nuevoProducto = new ProductosInfo( 10,"Crema facial hidratante", "Avon",800, "articulo10.jpg");
-const nuevoProducto2 = new ProductosInfo( 11,"Crema facial hidratante","Revlon",1100,"articulo10.jpg");
-//Function cargar productos al array principal
-
 function cargarArticulos(arr,element) {
     arr.push(element);
 }
 
 cargarArticulos(productos,nuevoProducto);
 cargarArticulos(productos,nuevoProducto2);
-console.log(productos);
 
-
-/*Function Busqueda por Nombre
-function busquedaporNombre(arr,filtro) {
-    const nombreBusqueda = arr.filter ((el) => {
-        return el.nombre.includes(filtro)
-    });
-    return nombreBusqueda;
-}  */
 
 /*flitrar por precio
 function filtrarporPrecio(arr,filtro){
@@ -49,47 +43,90 @@ function filtrarporPrecio(arr,filtro){
             return element.precio > 1000;
     }
    });
-} */
-
-// const precio = filtrarporPrecio(productos,filtroprecio);
- // console.log(precio)
-
- const cajaTarjetas = document.querySelector(".tarjetas");
- const cajaCarrito = document.querySelector(".carrito");
-
- function crearCards(){
-    productos.forEach(element=>{
-        cajaTarjetas.innerHTML += `<div class = "tarjeta">
-        <img src="../images/${element.img}" alt="">
-        <h4>${element.nombre}</h4>
-        <p>$${element.precio}</p>
-        <button class= "btnCarrito" id="btn-agregar${element.id}" >Agregar</button>
-        </div>`
-    });
 }
+const precio = filtrarporPrecio(productos,filtroprecio);  */
+
+function busquedaporNombre(arr,filtro) {
+    const nombreBusqueda = arr.filter ((el) => {
+        return el.nombre.includes(filtro)
+    });
+    return nombreBusqueda;
+;}
+
+function crearHtml(arr) {
+    let html = "";
+    for ( element of productos) {
+      html += `<div class = "tarjeta">
+      <img src="../images/${element.img}" alt="">
+      <h4>${element.nombre}</h4>
+      <h6>${element.marca}</h6>
+      <p>$${element.precio}</p>
+      <button class= "btnCarrito" id="btn-agregar${element.id}" >Agregar</button>
+      </div>`
+  };
+      cajaTarjetas.innerHTML += html;
+      agregarFuncionAlBoton();
+    };
 
 
 function agregarFuncionAlBoton(){
     productos.forEach(element=>{
         document.querySelector(`#btn-agregar${element.id}`).addEventListener("click",()=>{
-            agregarAlCarrito(element) ;
+            agregarAlCarrito(element);
+        })
+    }) 
+};
+
+
+function agregarAlCarrito(element){ 
+    let existe = carrito.some(prod=>prod.id === element.id);
+
+    if( existe===false ){
+        element.cantidad = 1;
+        carrito.push(element);
+    }
+    else{
+        let prodFind = carrito.find( prod=> prod.id===element.id );
+        prodFind.cantidad++;
+    }
+    actualizarCarrito();
+};
+
+function actualizarCarrito(){
+    cajaCarrito.innerHTML = "";
+    carrito.forEach(prod=>{
+        cajaCarrito.innerHTML += `<div class ="carrito">
+        <h4>${prod.nombre}</h4>
+        <h3>CANTIDAD: ${prod.cantidad}</h3>
+        <p>$${prod.precio}</p>
+        <button class="btnCarrito" id="btn-borrar${prod.id}">Borrar</button>
+        </div>`
+    })
+    localStorage.setItem("carrito",JSON.stringify(carrito))
+    borrarProducto();
+}
+
+function borrarProducto(){
+    carrito.forEach(producto=>{
+        document.querySelector(`#btn-borrar${producto.id}`).addEventListener("click",()=>{
+            let indice = carrito.findIndex(element=>element.id===producto.id);
+            carrito.splice(indice,1);
+            actualizarCarrito()
         })
     })
 }
 
-function agregarAlCarrito(producto){
-    /* console.log(producto.id); */
-    let existe = carrito.some(prod=>prod.id === producto.id);
-    if(existe===false){
-        producto.cantidad = 1;
-        carrito.push(producto);
-    }
-    else{
-        let prodFind = carrito.find(prod=> prod.id===producto.id);
-        prodFind.cantidad++;
-    }
-    console.log(carrito);
-    }
 
-    crearCards();
-    agregarFuncionAlBoton(); 
+
+//listener Funciona pero no lo pinta en html;
+    search.addEventListener("click", (e) => {
+    e.preventDefault();
+    let nuevoFiltro =busquedaporNombre(productos,search.value);
+    console.log(nuevoFiltro);
+    cajaTarjetas.innerHTML = "";
+    crearHtml(nuevoFiltro);
+});
+
+actualizarCarrito();
+crearHtml();
+
