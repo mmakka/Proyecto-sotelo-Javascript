1dfcfde4cc96193e79f8d7456b6507ac0cad0051
macarena sotelo <macarenasotelo1@gmail.com>
2023-02-19 04:17:50 -1100

    arreglo errores

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 
- // Swal.fire("Bienvenido");
-
-const total= document.querySelector("#total");
+const total= document.querySelector(".cardTotal");
 const carrito= JSON.parse(localStorage.getItem("carrito")) || [];
+const cantidadCarrito = document.getElementById("cantidadCarrito")
 const cajaTarjetas = document.querySelector(".tarjetas");
 const cajaCarrito = document.querySelector(".carrito");
 const buscar = document.querySelector("#ingreso");
 const search = document.querySelector("#btnSearch");
 const nuevoProducto = new ProductosInfo( 10,"Crema facial hidratante", "Avon",800, "articulo10.jpg");
 const nuevoProducto2 = new ProductosInfo( 11,"Crema facial hidratante","Revlon",1100,"articulo10.jpg");
+const finalCompra = document.querySelector("#login");
  
 
 function ProductosInfo(id,nombre,marca,precio,img){
@@ -34,11 +34,12 @@ function crearHtml(arr) {
         <h4>${nombre}</h4>
         <h6>${marca}</h6>
         <p>$${precio}</p>
-        <button class= "btnCarrito" id="btn-agregar${element.id}" >Agregar</button>
+        <button class= "btnCarrito" id="btn-agregar${element.id}">Agregar</button>
         </div>`
     };
     cajaTarjetas.innerHTML += html;
     agregarFuncionAlBoton(arr);
+    
 };
 
 
@@ -46,11 +47,11 @@ function filtrarPorNombre(array){
     let nombre = buscar.value;
     let nombreC = nombre.charAt(0).toUpperCase() + nombre.slice(1);
     if (!nombre) {
-      return array;
+    return array;
     } else {
-      return array.filter((e) => e.nombre.includes(nombreC));
+    return array.filter((e) => e.nombre.includes(nombreC));
     }
-  }
+}
 
 
 function agregarFuncionAlBoton(productos){
@@ -62,6 +63,7 @@ function agregarFuncionAlBoton(productos){
 };
 
 
+
 function agregarAlCarrito(element){ 
     let existe = carrito.some(prod=>prod.id === element.id);
       if( existe===false ){
@@ -69,9 +71,10 @@ function agregarAlCarrito(element){
         carrito.push(element);
     }
     else{
-        let prodFind = carrito.find( prod=> prod.id===element.id );
-        prodFind.cantidad++;  
+        let prodFind = carrito.find( prod=> prod.id===element.id);
+        prodFind.cantidad ++; 
     } 
+  
     actualizarCarrito();
     sumarTotal();
 };
@@ -80,76 +83,70 @@ function agregarAlCarrito(element){
 function actualizarCarrito(){
     cajaCarrito.innerHTML = "";
     carrito.forEach(prod=>{
-        cajaCarrito.innerHTML += `<div class ="carrito">
+        cajaCarrito.innerHTML +=`<div class ="carrito">
         <div>
         <h4>${prod.nombre}</h4>
-        <h3>CANTIDAD: ${prod.cantidad}</h3>
-        <p>$${prod.precio}</p>
-        <button class="btnCarrito" id="btn-borrar${prod.id}">Eliminar</button>
+        <h3>CANTIDAD:${prod.cantidad}</h3>
+        <p>$${prod.precio * prod.cantidad}</p>
+        <button id="btn-borrar${prod.id}" class="btnCarrito">Eliminar</button>
         </div>
         <div>`
     })
-    
+   
     localStorage.setItem("carrito",JSON.stringify(carrito))
     borrarProducto();
     sumarTotal();
+   
 }
 
 
-function borrarProducto(){
-    carrito.forEach(producto=>{
-        document.querySelector(`#btn-borrar${producto.id}`).addEventListener("click",()=>{
-            let indice = carrito.findIndex(element=>element.id===producto.id);
-            carrito.splice(indice,1);
-            Swal.fire({
-                title: 'Estas seguro?',
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Si, estoy seguro!'
-              }).then((result) => {
-                if (result.isConfirmed) {
-                  Swal.fire(
-                    'Eliminado!',
-                  )
-                }
-              })
-            actualizarCarrito()
-        })
-    })
-};
 
+function borrarProducto(){
+    carrito.forEach(producto =>{
+        document.querySelector(`#btn-borrar${producto.id}`).addEventListener("click",()=>{
+            console.log("clickeaste");
+           let indice = carrito.findIndex(element=>element.id === producto.id);
+            carrito.splice(indice,1);
+            actualizarCarrito()
+              })  
+        })
+       
+}
 
 function sumarTotal(){
     let Total = 0;
     cardTotal= document.querySelector("#cardTotal");
     carrito.forEach((prod) =>{
         const precio = carrito.reduce((acc,prod)=> acc + prod.precio, 0 )
-        Total =  carrito.reduce ((acc,prod)=> acc + prod.cantidad * prod.precio , 0 )
+        Total =  carrito.reduce ((acc,prod)=> acc + prod.precio * prod.cantidad , 0 )
     });
-    cardTotal.innerHTML = `Total $${Total}`
+    cardTotal.innerHTML =`Total $${Total}`
 }
 
 
-    const fetchApi = async ()=>{
-    const response = await fetch(`./js/data.json`);
-    const data = await response.json();
-    crearHtml(data);
+const fetchApi = async ()=>{
+const response = await fetch(`./js/data.json`);
+const data = await response.json();
+crearHtml(data);
 
 
-    search.addEventListener("click", ()=>{
-        const filtro = filtrarPorNombre(data)
-        crearHtml(filtro)
+search.addEventListener("click", ()=>{
+    const filtro = filtrarPorNombre(data)
+    crearHtml(filtro)
     });
 }
-
-const finalCompra = document.querySelector("#login");
 
 finalCompra.addEventListener("click", ()=>{
       cajaCarrito.style.display = "none";
        cardTotal.style.display = "none";
 })
+
+//NO FUNCIONA
+const carritoCounter = ()=>{
+    cantidadCarrito.style.display= "block";
+    cantidadCarrito.innerText = carrito.lenght;
+}
+
 
 fetchApi();
 actualizarCarrito();
